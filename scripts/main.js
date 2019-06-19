0eeff70f188d77b5b940c7144c2d62a6ad2a7c97
avavion <avavionmvm@gmail.com>
2019-06-20 01:22:51 +0300

    Create menu, add adaptive menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,17 @@
-// First Debug
+// Preloader
 
+// document.body.addEventListener("load", () => {
+//   setTimeout(() => {
+// 	let preloader = document.querySelector(".preloader");
+// 	console.log('debug');
+	
+//     if (!preloader.classList.contains("done")) {
+//       preloader.classList.add("done");
+//     }
+//   }, 1000);
+// });
 
-console.log("Hello, world!");
+// Бургер-меню кнопка
+document.querySelector(".menu-list__button").addEventListener("click", () => {
+  document.querySelector(".menu-list").classList.toggle("menu-list-enable");
+});
